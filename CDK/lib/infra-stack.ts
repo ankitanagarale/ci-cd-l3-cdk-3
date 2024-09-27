958e630185b8d57eba8f4c04278f32aae560784c
subodh6 <46165221+subodh6@users.noreply.github.com>
2024-09-27 16:21:59 +0530

    Update infra-stack.ts

--- CDK/lib/infra-stack.ts
+++ CDK/lib/infra-stack.ts
@@ -1,98 +1,72 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import { Stack, StackProps, CfnOutput, Stage, StageProps } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep, CodeBuildStep } from 'aws-cdk-lib/pipelines';
-import { config } from './config'; // Assuming this imports configuration values
-import { MyPipelineAppStage } from './stage';
-import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+// import { Function, InlineCode, Runtime, Code} from 'aws-cdk-lib/aws-lambda';
+// import * as path from 'path';
 import * as iam from 'aws-cdk-lib/aws-iam';
-
-export class CiCdAwsPipelineDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      crossAccountKeys: true,
-      // enableKeyRotation: true,
-      pipelineName: 'TestPipeline',
-      synth: new CodeBuildStep('Build', {
-        input: CodePipelineSource.connection("subodh6/ci-cd-l3-cdk-3", "main", {
-          connectionArn: "arn:aws:codeconnections:us-east-1:264852106485:connection/34df75a1-47fe-460d-ad44-2b3f37911bc9",
-        }),
-        commands: [
-          'cp -r CDK Ohana-Springboot/',
-          'mv ./Ohana-Springboot/CDK ./Ohana-Springboot/build_artifacts',
-          'cd CDK',
-          'ls',
-          'npm ci', 'npm run build', 'npx cdk synth',
-          'cp -r cdk.out/* ../Ohana-Springboot/build_artifacts/',
-          'cd ..',
-          'cp -r aws/infra/codepipeline/* Ohana-Springboot/',
-          'cd Ohana-Springboot',
-          'ls',
-          'chmod +x test.sh',
-          './test.sh',
-          'ls -al',
-          ],
-        buildEnvironment: {
-          // buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
-          buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_3,
+import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import { config } from './config';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+ 
+ 
+ 
+export class MyInfraStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
+      super(scope, id, props);
+   
+      new s3.Bucket(this, 'CodePipelineBucket', {
+        bucketName: `${this.stackName}-lab`,
+        lifecycleRules: [{
+          id: config.bucketLifecyclePolicy.id,
+          enabled: config.bucketLifecyclePolicy.status === 'Enabled',
+          prefix: config.bucketLifecyclePolicy.prefix,
+          transitions: [{
+            storageClass: s3.StorageClass.INTELLIGENT_TIERING,
+            transitionAfter: cdk.Duration.days(config.bucketLifecyclePolicy.transitionInDays),
+          }],
+          expiration: cdk.Duration.days(config.bucketLifecyclePolicy.expirationInDays),
+        }],
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      });
+ 
+      // Create CodeDeploy Role with stack name in the role name
+      const codeDeployRole = new iam.Role(this, 'CodeDeployRole', {
+        roleName: `${this.stackName}-code-deploy-role`,
+        assumedBy: new iam.ServicePrincipal(`codedeploy.${cdk.Stack.of(this).region}.amazonaws.com`), // Dynamic region
+        inlinePolicies: {
+          CodeDeployPermissions: new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                actions: [
+                  "ec2:Describe*",
+                  "s3:Get*",
+                  "s3:List*",
+                ],
+                resources: ["*"],
+              }),
+            ],
+          }),
         },
-        primaryOutputDirectory: './Ohana-Springboot/build_artifacts',
-      }),
-      selfMutation: false
-    });
-
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
-      env: { account: "954503069243", region: "us-east-1" }
-    }));
-    // Create an IAM role for the CodeBuildStep
-    const testRole = new iam.Role(this, 'TestRole-cicd', {
-      assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
-      inlinePolicies: {
-        AssumeRolePolicy: new iam.PolicyDocument({
-          statements: [
-            new iam.PolicyStatement({
-              actions: ['sts:AssumeRole'],
-              resources: [
-                'arn:aws:iam::954503069243:role/cdk-hnb659fds-deploy-role-954503069243-us-east-1',
-                'arn:aws:iam::954503069243:role/cdk-hnb659fds-file-publishing-role-954503069243-us-east-1'
-              ],
-            }),
-            new iam.PolicyStatement({
-              actions: ['ssm:GetParameter', 'ssm:GetParameters', 'ssm:GetParametersByPath'],
-              resources: ['arn:aws:ssm:us-east-1:264852106485:parameter/matson-hello-world/*'],
-            }),
-          ],
+      });
+     
+      // Create CodeDeploy Application with stack name in the application name
+      const codeDeployApplication = new codedeploy.ServerApplication(this, 'CodeDeployApplication', {
+        applicationName: `${this.stackName}-application`,
+      });
+ 
+      // Create CodeDeploy Deployment Group with stack name in the deployment group name
+      new codedeploy.ServerDeploymentGroup(this, 'DeploymentGroup', {
+        application: codeDeployApplication,
+        deploymentGroupName: `${this.stackName}-deploygroup`, 
+        role: codeDeployRole,
+        ec2InstanceTags: new codedeploy.InstanceTagSet({
+          'Name': ['matson'], 
         }),
-      },
-    });
-
-    testingStage.addPost(new CodeBuildStep("Deploy Application", {
-      input: pipeline.synth,
-      primaryOutputDirectory: '',
-      commands: [ 'ls',
-                'chmod +x test2.sh',
-                './test2.sh',
-      ],
-      buildEnvironment: {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
-      },
-      env: {
-        STAGE: 'lab',
-        CROSS_ACCOUNT_S3_BUCKET: 'test-cross-teest-680-lab',
-        CROSS_ACCOUNT_S3_BUCKET_PATH: "s3://test-cross-teest-680-lab"
-      },
-      role: testRole 
-    }));
-
-    // testingStage.addPre(new CodeBuildStep("Run Unit Tests", { commands: ['npm install'],
-    //   role: testRole // Assign the role to the CodeBuildStep
-    //  }));
-    // testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
-
-    // const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
-    //   env: { account: "264852106485", region: "us-east-1" }
-    // }));
-  }
+        autoRollback: {
+          failedDeployment: true,
+        },
+        deploymentConfig: codedeploy.ServerDeploymentConfig.ALL_AT_ONCE,
+      });
+    }
+   
+   
 }
