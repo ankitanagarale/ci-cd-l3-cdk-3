28b549f26762dc1930cac86647673c8a4b730af3
ankitanagarale <131234134+ankitanagarale@users.noreply.github.com>
2024-10-08 12:11:10 +0530

    Update stage.ts

--- CDK/lib/stage.ts
+++ CDK/lib/stage.ts
@@ -6,6 +6,6 @@ export class MyPipelineAppStage extends cdk.Stage {
     
     constructor(scope: Construct, stageName: string, props?: cdk.StageProps) {
       super(scope, stageName, props);
-      new MyInfraStack(this, 'deploydevstage', stageName);      
+      new MyInfraStack(this, 'deploystage', stageName);      
     }
 }
